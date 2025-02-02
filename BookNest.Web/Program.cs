9027a5f04ab3791cdcec8e0d28596c6f5861787b
ErenSakrak <erensakrak@outlook.com>
2025-02-02 19:05:05 +0300

    Interface and Class Added and BookNest.Application Updated!

--- BookNest.Web/Program.cs
+++ BookNest.Web/Program.cs
@@ -1,4 +1,6 @@
+using BookNest.Application.Common.Interfaces;
 using BookNest.Infrastructure.Data;
+using BookNest.Infrastructure.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(option=>
 option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IVillaRepository,VillaRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
