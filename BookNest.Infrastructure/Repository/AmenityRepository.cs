952ffe9affb19323d7a3b6af29ecb39bd9a14b7a
ErenSakrak <erensakrak@outlook.com>
2025-02-02 21:25:10 +0300

    Amenity Repository Added!

---
+++ BookNest.Infrastructure/Repository/AmenityRepository.cs
@@ -0,0 +1,26 @@
+﻿using BookNest.Application.Common.Interfaces;
+using BookNest.Domain.Entities;
+using BookNest.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookNest.Infrastructure.Repository
+{
+    public class AmenityRepository : Repository<Amenity>, IAmenityRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public AmenityRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public void Update(Amenity entity)
+        {
+            _db.Amenities.Update(entity);
+        }
+    }
+}
