eeb961f89cbbd32403196e2483cb03d055134e54
ErenSakrak <erensakrak@outlook.com>
2025-02-02 16:47:45 +0300

    Admin Panel CRUD System Added!

---
+++ BookNest.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,55 @@
+﻿using BookNest.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookNest.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Villa> Villas { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Villa>().HasData(
+                new Villa
+                {
+                    Id = 1,
+                    Name = "Royal Villa",
+                    Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s Lorem Ipsum.",
+                    ImageUrl = "https://placehold.co/600x400",
+                    Occupancy = 4,
+                    Price = 200,
+                    Sqft = 550
+                },
+                new Villa
+                {
+                    Id = 2,
+                    Name = "Premium Pool Villa",
+                    Description = "Donec nec posuere tortor. Aenean quis condimentum arcu, non ultricies sapien. Etiam condimentum elementum commodo. Etiam pulvinar ante eget iaculis efficitur.",
+                    ImageUrl = "https://placehold.co/600x401",
+                    Occupancy = 4,
+                    Price = 300,
+                    Sqft = 650
+                },
+                new Villa
+                {
+                    Id = 3,
+                    Name = "Luxury Pool Villa",
+                    Description = "Vestibulum hendrerit diam eget est iaculis finibus. Suspendisse nec metus sed metus posuere porttitor. Quisque sed leo sapien. Phasellus hendrerit hendrerit rutrum. Donec ut porta neque. Quisque condimentum blandit pretium.",
+                    ImageUrl = "https://placehold.co/600x402",
+                    Occupancy = 4,
+                    Price = 450,
+                    Sqft = 750
+                }
+                );
+        }
+    }
+}
---
+++ BookNest.Infrastructure/Migrations/20250202113352_seedVillaToDB.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BookNest.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class seedVillaToDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Created_Date", "Description", "ImageUrl", "Name", "Occupancy", "Price", "Sqft", "Updated_Date" },
+                values: new object[,]
+                {
+                    { 1, null, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s Lorem Ipsum.", "https://placehold.co/600x400", "Royal Villa", 4, 200.0, 550, null },
+                    { 2, null, "Donec nec posuere tortor. Aenean quis condimentum arcu, non ultricies sapien. Etiam condimentum elementum commodo. Etiam pulvinar ante eget iaculis efficitur.", "https://placehold.co/600x401", "Premium Pool Villa", 4, 300.0, 650, null },
+                    { 3, null, "Vestibulum hendrerit diam eget est iaculis finibus. Suspendisse nec metus sed metus posuere porttitor. Quisque sed leo sapien. Phasellus hendrerit hendrerit rutrum. Donec ut porta neque. Quisque condimentum blandit pretium.", "https://placehold.co/600x402", "Luxury Pool Villa", 4, 450.0, 750, null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
