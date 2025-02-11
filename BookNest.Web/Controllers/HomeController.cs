b9e3be1da103a4e4abe8527c337548c862687637
ErenSakrak <erensakrak@outlook.com>
2025-02-11 14:16:51 +0300

    Home Page updated!

--- BookNest.Web/Controllers/HomeController.cs
+++ BookNest.Web/Controllers/HomeController.cs
@@ -1,21 +1,29 @@
 using System.Diagnostics;
+using BookNest.Application.Common.Interfaces;
 using BookNest.Web.Models;
+using BookNest.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookNest.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IUnitOfWork unitOfWork)
         {
-            _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeVM homeVM = new()
+            {
+                VillaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity"),
+                Nights = 1,
+                CheckInDate = DateOnly.FromDateTime(DateTime.Now),
+            };
+            return View(homeVM);
         }
 
         public IActionResult Privacy()
