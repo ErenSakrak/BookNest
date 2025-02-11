22e3a0c45ad9050858ab0f81eebc316e93271e34
ErenSakrak <erensakrak@outlook.com>
2025-02-11 15:01:51 +0300

    Domain/Entities Updated !

--- BookNest.Domain/Entities/Amenity.cs
+++ BookNest.Domain/Entities/Amenity.cs
@@ -13,15 +13,15 @@ namespace BookNest.Domain.Entities
     {
         [Key]
         public int Id { get; set; }
-
+        
         public required string Name { get; set; }
         public string? Description { get; set; }
 
+
         [ForeignKey("Villa")]
         public int VillaId { get; set; }
-
         [ValidateNever]
         public Villa Villa { get; set; }
-
+        
     }
 }
